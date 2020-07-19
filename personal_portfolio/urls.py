e4889aa663fb58a8c5746f36d404d27507312570
shirishsoni <shirishsoni12@gmail.com>
2020-07-19 18:02:04 +0530

    Updated urls.py

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,7 +22,6 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
